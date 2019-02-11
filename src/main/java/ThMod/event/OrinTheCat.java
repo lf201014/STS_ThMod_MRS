9324a5cab4743895324d24a0a17c2ba001db4ac8
Flynn1014 <lf201014@163.com>
2019-02-11 11:39:54 +0800

    orin event final!!!!!!!!!

--- src/main/java/ThMod/event/OrinTheCat.java
+++ src/main/java/ThMod/event/OrinTheCat.java
@@ -1,8 +1,11 @@
 package ThMod.event;
 
+import static ThMod.ThMod.logger;
+
 import ThMod.ThMod;
 import ThMod.cards.derivations.Wraith;
 import ThMod.relics.CatCart;
+import com.megacrit.cardcrawl.actions.common.EscapeAction;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -11,6 +14,7 @@ import com.megacrit.cardcrawl.events.RoomEventDialog;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.helpers.MonsterHelper;
 import com.megacrit.cardcrawl.localization.EventStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
 
 public class OrinTheCat
@@ -23,6 +27,8 @@ public class OrinTheCat
   private static final String[] OPTIONS = eventStrings.OPTIONS;
   private static final String INTRO_MSG = DESCRIPTIONS[0];
   private CurScreen screen = CurScreen.INTRO;
+  private AbstractMonster orin;
+  private boolean satori = false;
 
   enum CurScreen {
     INTRO, PRE_COMBAT, END
@@ -36,14 +42,24 @@ public class OrinTheCat
     );
 
     this.roomEventText.clear();
-
     this.body = INTRO_MSG;
-    this.roomEventText.addDialogOption(OPTIONS[0], CardLibrary.getCopy("Wraith"));
+    satori = AbstractDungeon.player.name.equals("Komeiji");
+    if (satori) {
+      this.roomEventText.addDialogOption(OPTIONS[4]);
+    } else {
+      this.roomEventText.addDialogOption(OPTIONS[0], CardLibrary.getCopy("Wraith"));
+    }
     this.roomEventText.addDialogOption(OPTIONS[1]);
 
     this.hasDialog = true;
     this.hasFocus = true;
     AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("Orin");
+    orin = AbstractDungeon.getCurrRoom().monsters.monsters.get(0);
+    if (orin != null) {
+      logger.info("OrinTheCat : orin get : " + orin.name);
+    } else {
+      logger.info("OrinTheCat : error : null orin");
+    }
   }
 
   public void update() {
@@ -58,28 +74,50 @@ public class OrinTheCat
       case INTRO:
         switch (buttonPressed) {
           case 0:
-            this.screen = CurScreen.END;
-            this.roomEventText.updateBodyText(DESCRIPTIONS[2]);
-            this.roomEventText.updateDialogOption(0, OPTIONS[3]);
-            this.roomEventText.clearRemainingOptions();
-            AbstractDungeon.effectList.add(
-                new ShowCardAndObtainEffect(
-                    new Wraith(),
-                    Settings.WIDTH / 2.0F,
-                    Settings.HEIGHT / 2.0F
-                )
-            );
-            logMetricIgnored(ID);
-            return;
+            if (satori) {
+              this.screen = CurScreen.END;
+              AbstractDungeon.getCurrRoom().spawnRelicAndObtain(
+                  (float) (Settings.WIDTH / 2), (float) (Settings.HEIGHT / 2), new CatCart()
+              );
+              this.roomEventText.updateBodyText(DESCRIPTIONS[3]);
+              this.roomEventText.updateDialogOption(0, OPTIONS[3]);
+              this.roomEventText.clearRemainingOptions();
+              return;
+            } else {
+              this.screen = CurScreen.END;
+              this.roomEventText.updateBodyText(DESCRIPTIONS[2]);
+              this.roomEventText.updateDialogOption(0, OPTIONS[3]);
+              this.roomEventText.clearRemainingOptions();
+              AbstractDungeon.effectList.add(
+                  new ShowCardAndObtainEffect(
+                      new Wraith(),
+                      Settings.WIDTH / 2.0F,
+                      Settings.HEIGHT / 2.0F
+                  )
+              );
+              logMetricIgnored(ID);
+              return;
+            }
           case 1:
-            this.screen = CurScreen.PRE_COMBAT;
-            this.roomEventText.updateBodyText(DESCRIPTIONS[1]);
-            this.roomEventText.updateDialogOption(0, OPTIONS[2]);
-            this.roomEventText.clearRemainingOptions();
-            logMetric(ID, "Fight");
-            return;
+            if (satori) {
+              this.screen = CurScreen.END;
+              if (orin != null) {
+                AbstractDungeon.actionManager.addToBottom(new EscapeAction(orin));
+              }
+              this.roomEventText.updateBodyText(DESCRIPTIONS[4]);
+              this.roomEventText.updateDialogOption(0, OPTIONS[3]);
+              this.roomEventText.clearRemainingOptions();
+              return;
+            } else {
+              this.screen = CurScreen.PRE_COMBAT;
+              this.roomEventText.updateBodyText(DESCRIPTIONS[1]);
+              this.roomEventText.updateDialogOption(0, OPTIONS[2]);
+              this.roomEventText.clearRemainingOptions();
+              logMetric(ID, "Fight");
+              return;
+            }
           default:
-            ThMod.logger.info(
+            logger.info(
                 "OrinTheCat event : error : key number "
                     + buttonPressed
                     + " should never be pressed."
